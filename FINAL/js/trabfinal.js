c56f2fc7df93dc769aab22bfcf7c280d14c297e5
Diego Becker <diegoluizbecker@gmail.com>
2020-12-18 11:37:54 -0300

    Add carro

--- FINAL/js/trabfinal.js
+++ FINAL/js/trabfinal.js
@@ -1,17 +1,19 @@
-var scene;
-var camera;
-var renderer;
-var createFloor;
+var scene,camera, renderer, createFloor, loadObj, activeAction, lastAction;
 
-var material;
-var floor;
+var animationActions = Array();
 
-var cube;
+var material,floor;
+var pontos = 0;
 
+var iniciar = false;
 
-var render = function() {
+var objetosCarregados = [];
+
+var render = () => {
     requestAnimationFrame(render);
-    renderer.render( scene, camera );
+    //document.getElementById("points").innerHTML = pontos + " Pontos";
+    //pontos++;
+    renderer.render(scene, camera);
 };
 
 createFloor = () =>{
@@ -34,20 +36,107 @@ createFloor = () =>{
     scene.add(floor);
 };
 
+loadObj = () =>{
+    var fbxLoader = new THREE.FBXLoader();
+    var textureLoader = new THREE.TextureLoader();
+    fbxLoader.load('assets/models/Police Car.fbx', (object) =>{
+            objetosCarregados[0] = object;
+
+            object.traverse( function ( child ) {
+                if ( child instanceof THREE.Mesh ) {
+                    console.log(child);
+                    child.material.map = textureLoader.load("assets/models/UVPoliceCar.png");
+                    child.material.shininess = 0;
+                    child.castShadow = true;
+                    child.receiveShadow = true;
+                }
+            });
+
+            object.scale.x = 0.01;
+            object.scale.y = 0.01;
+            object.scale.z = 0.01;
+
+            object.position.z = 0;
+            object.position.x = 0;
+            object.position.y = 0;
+
+
+            object.rotation.y += 6.3;
+
+            object.castShadow = true;
+
+            scene.add(object);    
+        },(andamento) => {
+            console.log((andamento.loaded / andamento.total *100) + "% pronto!");
+        },(error) => {
+            console.log("Deu caca: " + error);
+        });
+};
+
 window.onload = () =>{
+
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0xcce0ff);
-    camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 10000);
 
-    renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.shadowMap.enabled = true;
-    document.body.appendChild(renderer.domElement);
 
+    document.body.appendChild(renderer.domElement);
+    
+    camera.position.x = 0;
+    camera.position.z = 10;
+    camera.position.y = 2;
+	scene.add(camera);
+    
     //criação do chão
     createFloor();
+    
+    var light = new THREE.PointLight(0xFFFFFF);
+	light.position.set(1, 10, 40);
+	scene.add(light);
 
-    camera.position.z = 5;
+
+    loadObj();
+
+    document.addEventListener('keydown', (event)=>{
+        event = event || window.event;
+		var keycode = event.keyCode;
+		switch(keycode)
+		{
+			case 37:
+				console.log("Apertou seta esquerda");
+				break;
+			case 38:
+				console.log("Apertou seta cima");
+				break;
+			case 39:
+				console.log("Apertou seta direita");
+				break;
+			case 40:
+				console.log("Apertou seta baixo");
+				break;
+			}
+    });
+	document.addEventListener('keyup', () =>{
+        event = event || window.event;
+		var keycode = event.keyCode;
+		switch(keycode)
+		{
+			case 37:
+				console.log("Largou esq");
+				break;
+			case 38:
+				console.log("Largou cima");
+				break;
+			case 39:
+				console.log("Largou direita");
+				break;
+			case 40:
+				console.log("Largou baixo");
+				break;
+			}
+    });
 
     render();
 }
